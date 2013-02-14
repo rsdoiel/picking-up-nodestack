0ad76a81edc2933e39d8d0856a38f5725f0df274
R. S. Doiel <rsdoiel@gmail.com>
2013-02-14 07:03:57 -0800

    re-organizing text

--- exercise-03/server.js
+++ exercise-03/server.js
@@ -1,25 +1,20 @@
-	var fs = require("fs"),
-	        // Notice we don't need to include http directly, that is already inside express
-	        express = require("express"),
-	        // Read in our pages 
-	        homepage = fs.readFileSync("index.html").toString(),
-		errorpage = fs.readFileSync("404.html").toString(),
-	        // create our web server
+	var express = require("express"),
 	        server = express();
 
-	// Now we need to define our routes, first our homepage then everything else
+	// Now we need to define our routes. 
+	// First we define our homepage then everything else.
 	server.get("/", function (request, response) {
 	        console.log(request.url, 200, "found");
 		// Notice we're using .send instead of .end
-	        response.send(200, homepage);
+	        response.status(200).sendfile("index.html");
 	});
 	
 	// Here's a catch all route, in this case treating them as a 404.
 	// You could also do other things (e.g. redirect to a CDN)
 	server.get("*", function (request, response) {
 	        console.error(request.url, "not found");
-	        response.send(404, errorpage);
+	        response.status(404).sendfile("404.html");
 	});
 
-	console.log("Starting web server on port 3000");
+	console.log("Starting web server on port",3000);
 	server.listen(3000);
